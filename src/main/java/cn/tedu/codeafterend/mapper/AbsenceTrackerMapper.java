9d6f9b57f81960406e6314d631cd484dfad876fd
USER <bee31686@gmail.com>
2023-05-24 11:44:20 +0800

    新增出缺勤功能的備註。

--- src/main/java/cn/tedu/codeafterend/mapper/AbsenceTrackerMapper.java
+++ src/main/java/cn/tedu/codeafterend/mapper/AbsenceTrackerMapper.java
@@ -13,25 +13,70 @@ import java.util.List;
 @Repository
 public interface AbsenceTrackerMapper {
 
+    /**
+     * 新增出缺勤資料
+     * @param absenceTracker 出缺勤資料
+     */
     int insert(AbsenceTracker absenceTracker);
 
+    /**
+     * 根據ID刪除資料
+     * @param id 資料id
+     */
     int deleteById(Long id);
 
+    /**
+     * 根據ID修改資料
+     * @param absenceTracker 要修改的資料
+     */
     int updateById(AbsenceTracker absenceTracker);
 
+    /**
+     * 查詢所有出缺勤內容
+     * @return 返回所有出缺勤資料
+     */
     List<AbsenceTrackerVO> list();
 
+    /**
+     * 根據ID查詢資料並判斷是否需要根據月份查詢
+     * @param id 員工id
+     * @param month 月份
+     * @return 返回查詢資料
+     */
     List<AbsenceTrackerVO>  getAllById(@Param("id")Long id, @Param("month")String month);
 
+    /**
+     * 根據月份查詢出缺勤資料
+     * @param month 查詢月份
+     * @return 返回根據月份查詢的資料
+     */
     List<AbsenceTrackerVO>  getByMonthSelectList(String month);
 
+    /**
+     * 根據id查詢出缺勤資料
+     * @param id 查詢id
+     * @return 返回根據id查詢的資料
+     */
     AbsenceTrackerVO getStandardById(Long id);
 
+    /**
+     * 查詢員工資料
+     * @return 返回所有員工資料
+     */
     List<EmployeeDataStandardVO> selectEmployees();
 
+    /**
+     * 根據員工id刪除出缺勤資料
+     * @return 返回刪除筆數
+     */
     int deleteByEmployee(Long id);
 
-    AbsenceTrackerVO   getByAbsenceTrackerCalculate(Long id);
+    /**
+     * 根據員工id統計每個員工前一個月的請假時數
+     * @param id 員工id
+     * @return 返回計算的資料
+     */
+    AbsenceTrackerVO getByAbsenceTrackerCalculate(Long id);
 
 
 }
--- src/main/java/cn/tedu/codeafterend/service/IAbsenceTrackerService.java
+++ src/main/java/cn/tedu/codeafterend/service/IAbsenceTrackerService.java
@@ -13,23 +13,63 @@ import java.util.List;
 @Transactional
 public interface IAbsenceTrackerService {
     /**
+     * 新增出缺勤資料
      *
-     * @param absenceTrackerAddNewDTO
+     * @param absenceTrackerAddNewDTO 出缺勤資料
      */
     void addNew(AbsenceTrackerAddNewDTO absenceTrackerAddNewDTO);
 
+    /**
+     * 根據ID刪除資料
+     *
+     * @param id 資料id
+     */
     void deleteById(Long id);
 
+    /**
+     * 根據ID修改資料
+     *
+     * @param absenceTrackerAddNewDTO 要修改的資料
+     */
     void updateById(AbsenceTrackerAddNewDTO absenceTrackerAddNewDTO);
 
+    /**
+     * 查詢所有出缺勤內容
+     *
+     * @return 返回出缺勤資料
+     */
     List<AbsenceTrackerVO> list();
 
-    List<AbsenceTrackerVO> getAllById( Long id, String month);
+    /**
+     * 根據ID查詢資料並判斷是否需要根據月份查詢
+     *
+     * @param id 員工id
+     * @param month 月份
+     * @return 返回查詢資料
+     */
+    List<AbsenceTrackerVO> getAllById(Long id, String month);
 
-    List<AbsenceTrackerVO>  getByMonthSelectList(String month);
+    /**
+     * 根據月份查詢出缺勤資料
+     *
+     * @param month 查詢月份
+     * @return 返回根據月份查詢的資料
+     */
+    List<AbsenceTrackerVO> getByMonthSelectList(String month);
 
+    /**
+     * 根據id查詢出缺勤資料
+     *
+     * @param id 查詢id
+     * @return 返回根據id查詢的資料
+     */
     AbsenceTrackerVO selectById(Long id);
 
+    /**
+     * 查詢員工資料
+     *
+     * @return 返回所有員工資料
+     */
     List<EmployeeDataStandardVO> selectEmployees();
 
 
